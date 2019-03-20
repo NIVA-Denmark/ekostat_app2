a177eb2028712cd748d2d198768f4642e29b2d92
ciaran-dk <ciaran@murraynet.dk>
2019-03-20 20:25:16 +0100

    updated downloadResults function to match latest aggregation methods

--- download.R
+++ download.R
@@ -2,58 +2,85 @@
 downloadResults<-function(resMC,resAvg){
   
   withProgress(message = 'Preparing download...', value = 0, {
-    #resMC <- values$resMC
-    #resAvg <- values$resAvg
     
-    res1MC <-Aggregate(resMC,Groups=c("WB_ID", "Type", "Period", "sim"),level=1) %>%
-      rename(ClassMC = Class)
-    res1Avg <-Aggregate(resAvg,Groups = c("WB_ID", "Type", "Period"),level = 1) %>%
-      select(WB_ID, Type, Period, Class)
-    res1MC <- res1MC %>% left_join(res1Avg,by=c("WB_ID", "Type", "Period"))
+    res1Avg <-
+      Aggregate(
+        resAvg,
+        Groups = c("Period"),
+        level = 1
+      ) 
+    
+    res1MC <- AggregateMCovr(resMC,resAvg,res1Avg)
+    res1MC <- res1MC %>%
+      select(Period,sim,ClassID,ClassMC)
+    
+    res1MC <- res1MC %>% left_join(res1Avg,by = c("Period"))
+    
+    res1Avg <- res1Avg %>%
+      select(Period, Class)
     incProgress(0.1)
     
-    res2MC <- Aggregate(resMC,Groups = c("WB_ID", "Period", "Type", "sim"),level = 2) %>%
+    
+    res2MC<-AggregateMC(resMC,resAvg)
+    
+    res2Avg <-
+      Aggregate(resAvg,
+                Groups = c("Period"),
+                level = 2) %>%
+      select(Period, QEtype, EQR, Class)
+    
+    res2MC <- res2MC %>% left_join(res2Avg,by = c("Period", "QEtype"))
+    incProgress(0.1)
+    
+    res3Avg <-
+      Aggregate(resAvg,
+                Groups = c("Period"),
+                level = 3) %>%
+      select(Period, QEtype, QualityElement, EQR, Class)
+    
+    res3MC <- Aggregate(resMC,
+                        Groups = c("Period", "sim"),
+                        level = 3) %>%
       rename(ClassMC = Class, EQRMC = EQR)
-    res2Avg <- Aggregate(resAvg,Groups = c("WB_ID", "Period", "Type"),level = 2) %>% select(WB_ID, Type, Period, QEtype, EQR, Class)
-    res2MC <- res2MC %>% left_join(res2Avg,by = c("WB_ID", "Period", "Type", "QEtype"))
+    
+    res3MC <- res3MC %>% left_join(res3Avg,by = c("Period", "QEtype","QualityElement"))
     incProgress(0.1)
     
-    res3MC <-Aggregate(resMC,Groups = c("WB_ID", "Period", "Type", "sim"),level = 3) %>%
+    
+    res4MC <-Aggregate(resMC,Groups = c("Period", "sim"),level = 4) %>%
       rename(ClassMC = Class, EQRMC = EQR)
-    res3Avg <-Aggregate(resAvg,Groups = c("WB_ID", "Period", "Type"),level=3) %>%
-      select(WB_ID, Type, Period, QEtype, QualityElement, EQR, Class)
-    res3MC <- res3MC %>% left_join(res3Avg,by = c("WB_ID", "Period", "Type", "QualityElement", "QEtype"))
-    incProgress(0.1)
     
-    res4MC <-Aggregate(resMC,Groups = c("WB_ID", "Period", "Type", "sim"),level = 4) %>%
-      rename(ClassMC = Class, EQRMC = EQR)
-    res4Avg <-Aggregate(resAvg,Groups = c("WB_ID", "Period", "Type"),level = 4) %>%
-      select(WB_ID,Type,Period,QEtype,QualityElement,QualitySubelement,EQR,Class)
-    res4MC <- res4MC %>% left_join(res4Avg,by = c("WB_ID", "Period", "Type", "QualityElement", "QEtype", "QualitySubelement"))
+    res4Avg <-Aggregate(resAvg,Groups = c("Period"),level = 4) %>%
+      select(Period,QEtype,QualityElement,QualitySubelement,EQR,Class)
+    
+    res4MC <- res4MC %>% left_join(res4Avg,by = c("Period", "QualityElement", "QEtype", "QualitySubelement"))
     incProgress(0.1)
     
     
-    grplist <- c(  "WB_ID","Type","Period","QEtype","QualityElement","QualitySubelement","Indicator","IndSubtype",
-                   "Note","Unit","Months","Worst","PB","MP","GM","HG","Ref","Mean","StdErr","EQR","Class")
+    
+    
+    grplist <- c("Period","QEtype","QualityElement","QualitySubelement","Indicator","IndSubtype",
+                 "Note","Unit","Months","Worst","PB","MP","GM","HG","Ref","Mean","StdErr","EQR","Class")
     resMC <-resMC %>% rename(EQRMC = EQR,ClassMC = Class,Class = ClassAvg,EQR = EQRavg)
     
     resMC <- 
       SummarizeSims(resMC,Groups=grplist,ClassVar="ClassMC")
     resMC <- resMC %>% mutate(id=as.numeric(rownames(resMC)))
-    
-    nr<-nrow(resMC)
     incProgress(0.1)
     
-    grplist <-c("WB_ID", "Type", "Period", "Class")
-    rmlist = c("WB_ID", "Type")
+    nr<-nrow(resMC)
+    
+    rmlist <- ""
+    grplist <-c("Period", "Class")
+    
     res1MC <-
       SummarizeSims(res1MC,Groups=grplist , roundlist = c("pGES"),remove = rmlist,ClassVar = "ClassMC") 
     res1MC <- res1MC %>% mutate(id1=as.numeric(rownames(res1MC)))
     nr1<-nrow(res1MC)
     incProgress(0.1)
     
-    grplist <- c("WB_ID", "Period", "Type", "QEtype", "EQR", "Class")
-    rmlist = c("WB_ID", "Period", "Type")
+    grplist <- c("Period", "QEtype", "EQR", "Class")
+    rmlist = c("Period")
     
     res2MC <- 
       SummarizeSims(res2MC,Groups = grplist,remove = rmlist, ClassVar = "ClassMC")  
@@ -64,8 +91,8 @@ downloadResults<-function(resMC,resAvg){
     incProgress(0.1)
     
     grplist <-
-      c("WB_ID","Period","Type","QEtype","QualityElement","EQR","Class")
-    rmlist = c("WB_ID", "Period", "Type", "QEtype")
+      c("Period","QEtype","QualityElement","EQR","Class")
+    rmlist = c("Period", "QEtype")
     
     res3MC <-
       SummarizeSims(res3MC,roundlist = c("EQR","pGES"),Groups = grplist,remove = rmlist,ClassVar = "ClassMC")  
@@ -75,8 +102,8 @@ downloadResults<-function(resMC,resAvg){
     
     
     grplist <-
-      c("WB_ID","Period","Type","QEtype","QualityElement","QualitySubelement","EQR","Class")
-    rmlist = c("WB_ID", "Period", "Type", "QEtype", "QualityElement")
+      c("Period","QEtype","QualityElement","QualitySubelement","EQR","Class")
+    rmlist = c("Period", "QEtype", "QualityElement")
     
     
     res4MC <-
@@ -89,10 +116,18 @@ downloadResults<-function(resMC,resAvg){
     res2MC$id2<-res2MC$id2*nr*nr4*nr3
     res1MC$id1<-res1MC$id1*nr*nr4*nr3*nr2
     
-    res2MC <- res2MC %>% left_join(select(res1MC,Period,id1))
-    res3MC <- res3MC %>% left_join(select(res2MC,Period,QEtype,id1,id2))
-    res4MC <- res4MC %>% left_join(select(res3MC,Period,QEtype,QualityElement,id1,id2,id3))
-    resMC <- resMC %>% left_join(select(res4MC,Period,QEtype,QualityElement,QualitySubelement,id1,id2,id3,id4))
+    res2MC <- res2MC %>% 
+      left_join(select(res1MC,Period,id1),
+                by="Period")
+    res3MC <- res3MC %>% 
+      left_join(select(res2MC,Period,QEtype,id1,id2),
+                by=c("Period","QEtype"))
+    res4MC <- res4MC %>% 
+      left_join(select(res3MC,Period,QEtype,QualityElement,id1,id2,id3),
+                by=c("Period","QEtype","QualityElement"))
+    resMC <- resMC %>% 
+      left_join(select(res4MC,Period,QEtype,QualityElement,QualitySubelement,id1,id2,id3,id4),
+                by=c("Period","QEtype","QualityElement","QualitySubelement"))
     
     resMC <- resMC %>% rename(EQR_ind=EQR)
     res3MC <- res3MC %>% rename(EQR_QE=EQR)
